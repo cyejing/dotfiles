14ca3fba64c2b27f04a275de8dceaaf88d8c3aff
born <jing23501@qq.com>
2023-02-28 11:37:42 +0800

    del

--- sync_in.sh
+++ sync_in.sh
@@ -8,12 +8,15 @@ function doIt() {
         --include ".ideavimrc" \
         --include ".m2" \
         --include ".m2/settings.xml" \
+        --include ".ssh" \
+        --include ".ssh/config" \
         --include ".npmrc" \
         --include ".p10k.zsh" \
         --include ".vimrc" \
         --exclude "*" \
 		-avh --no-perms ~/ .;
 }
+
 
 doIt;
 unset doIt;
